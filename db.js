f13714becf05ebd06f6c9d0f05072f99d36fb660
JagadevaAS <94455225+JagadevaAS@users.noreply.github.com>
2021-12-07 22:35:33 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,23 @@
+// const mysql = require('mysql');
+// const express = require('express');
+// const app = express();
+
+
+
+// function connect(){}
+//         var con = mysql.createConnection({
+//           host: "localhost",
+//           user: "root",
+//           password: "password@123",
+//           database: "cricket"
+//         });
+
+//         con.connect(function(err) {
+//             if (err) throw err;
+            
+//               if (err) throw err;
+//           });
+    
+    
+
+//       module.exports = connect;
