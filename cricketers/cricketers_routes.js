ecf2365dff4d32bb6a743540480d5ffa8ba46ad5
JagadevaAS <94455225+JagadevaAS@users.noreply.github.com>
2021-12-12 17:33:54 +0530

    Delete cricketers directory

--- cricketers/cricketers_routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const db = require("../db");
-const players = require("./cricketers_ctrl");
-
-app.use("/",players);
-
-
-module.exports = app;
