1d7553955efbfa69e602e4d398cd25f78144dfd6
JagadevaAS <94455225+JagadevaAS@users.noreply.github.com>
2021-12-12 17:34:22 +0530

    Delete score directory

--- score/score_routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const db = require("../db");
-const score = require("../score/score_ctrl");
-
-app.use("/",score);
-
-module.exports = app;
